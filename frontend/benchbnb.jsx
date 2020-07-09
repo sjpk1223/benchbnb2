1030dc282de6b3f471a58081e145d4d60b58d7c1
joshua <sjpk1223@gmail.com>
2020-07-09 09:53:38 -0400

    Store completed

--- frontend/benchbnb.jsx
+++ frontend/benchbnb.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const store = configureStore();
+window.getState = store.getState;
+window.dispatch = store.dispatch;
+
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
     ReactDOM.render(<h1>Welcome to BenchBnB!</h1>, root);
 });
+

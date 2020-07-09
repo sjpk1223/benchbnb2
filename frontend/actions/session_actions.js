2cc66e58d2e8f61acba254a3dea9ef10375a8cce
joshua <sjpk1223@gmail.com>
2020-07-08 21:05:37 -0400

    Session action complete

---
+++ frontend/actions/session_actions.js
@@ -0,0 +1,31 @@
+import {
+    postSession,
+    postUser,
+    deleteSession
+} from '../util/session_api_util'
+
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER"
+export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER"
+export const RECEIVE_ERRORS = 'RECEIVE_ERRORS'
+
+const receiveErrors = (errors) => ({
+    type: RECEIVE_ERRORS,
+    errors: errors
+})
+const receiveCurrentUser = (user) => ({
+    type: RECEIVE_CURRENT_USER,
+    user: user
+})
+
+const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER,
+})
+
+export const createNewUser = (formUser) => dispatch => postUser(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const login = (formUser) => dispatch => postSession(formUser)
+    .then(user => dispatch(receiveCurrentUser(user)));
+
+export const logout = () => dispatch => deleteSession()
+    .then(() => dispatch(logoutCurrentUser()));

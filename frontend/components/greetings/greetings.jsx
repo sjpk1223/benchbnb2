b33fe8f173046e72091805a3f00aa81986418a6d
joshua <sjpk1223@gmail.com>
2020-07-11 20:09:18 -0400

    Greetings container testing incomplete

--- frontend/components/greetings/greetings.jsx
+++ frontend/components/greetings/greetings.jsx
@@ -5,13 +5,37 @@ import { Link } from 'react-router-dom'
 class Greetings extends React.Component{
     constructor(props){
         super(props)
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(type) {
+        //default action for a button in a form will send post request whcih will cause th page to re-render
+        // frontend state & backend state are not the same!
+        return (e) => {
+            e.preventDefault();
+        }
     }
 
     render(){
-        const { currentUser } = this.props;
+        const { currentUser, logout } = this.props;
+        const display = currentUser ? (
+            <div>
+                <p>Hello, {currentUser.username}</p>
+                <button onClick={logout}>Log Out</button>
+            </div>
+        ) : (
+                <>
+                    <button onClick={this.handleSubmit("login")} >
+                        Log In
+        </button>
+                    <button onClick={this.handleSubmit("signup")} >
+                        Sign Up
+        </button>
+                </>
+            );
         return (
             <div>
-                <h1>Hi!</h1>
+                {display}
             </div>
         );
     }

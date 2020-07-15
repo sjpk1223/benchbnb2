f8959af5db9d8710be2ebd58465640751216ae44
joshua <sjpk1223@gmail.com>
2020-07-15 01:38:18 -0400

    Session route complete

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import GreetingsContainer from "./greetings/greetings_container";
-
+import SignupContainer from "./session/signup_container";
+import LoginContainer from "./session/login_container";
+import {Route} from 'react-router-dom';
 const App = () => (
     <div>
         <header>
-            <GreetingsContainer />
+            <Route path='/' component={GreetingsContainer} />
+            <Route path='/signup' component={SignupContainer} />
+            <Route path='/login' component={SignupContainer} />
         </header>
     </div>
 );

a685ea5d7882f277f5436e4a4330913c140b8a54
joshua <sjpk1223@gmail.com>
2020-07-17 15:28:32 -0400

    Protectedroute imported

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -15,10 +15,26 @@ const Auth = ({ component: Component, path, loggedIn, exact }) => (
 const mapStateToProps = state => {
     return { loggedIn: Boolean(state.session.currentUser) };
 };
+
+const Protected = ({ loggedIn, path, component: Component }) => {
+    <Route 
+    path={path}
+    render={props => {
+        loggedIn ? <Component {...props} /> : <Redirect to="signup"/> 
+
+    }}
+    />
+}
 // withRouter gives access to location, history and match?
 export const AuthRoute = withRouter(
     connect(
         mapStateToProps,
         null
     )(Auth)
+);
+
+export const ProtectedRoute = withRouter(
+    connect(
+        (mapStateToProps)(Protected)
+    )
 );

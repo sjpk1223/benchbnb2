942886c7a6fa21584c481e8c6cdaad1658fb8a25
joshua <sjpk1223@gmail.com>
2020-07-29 19:39:16 -0400

    Started back end prep and completed bench model method

--- app/models/bench.rb
+++ app/models/bench.rb
@@ -1,3 +1,21 @@
 class Bench < ApplicationRecord
     validates :description, :lat, :lng, presence: true
+
+    # def self.in_bounds(bounds)
+    #     Bench.where([bounds: {northEast, southWest}]);
+    # end
+    
+    def self.in_bounds(bounds)
+        self.where("lat < ?", bounds[:northEast][:lat])
+        .where("lat > ?", bounds[:southWest][:lat])
+        .where("lng > ?", bounds[:southWest][:lng])
+        .where("lng < ?", bounds[:northEast][:lng])
+    end
+
+      # google map bounds will be in the following format:
+  # {
+  #   "northEast"=> {"lat"=>"37.80971", "lng"=>"-122.39208"},
+  #   "southWest"=> {"lat"=>"37.74187", "lng"=>"-122.47791"}
+  # }
+  #... query logic goes here
 end

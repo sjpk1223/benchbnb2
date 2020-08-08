7f5550c909da763e8b86b70a610d89b0103b12e0
joshua <sjpk1223@gmail.com>
2020-08-08 14:40:02 -0400

    Sort components

---
+++ frontend/components/search/review_form_container.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class ReviewForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            rating: 10,
+            body: ''
+        };
+    }
+
+    render(){
+        return (
+          <div></div>  
+        );
+    }
+}
